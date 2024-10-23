d2ed8d5d559dd58e143f49f9d16b66e64e5febfa
Ronald Yonggi <ronaldyonggi@gmail.com>
2024-10-23 19:53:24 +0800

    initial main

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,12 @@
+import './styles/reset.css';
+import './styles/global.css';
+
 import { createRoot } from 'react-dom/client';
+import ProfileCard from './components/ProfileCard/ProfileCard';
+import ActivityCard from './components/ActivityCard/ActivityCard';
+
+export function App() {
+  return <ActivityCard title='Work' timeframe='Daily' current={5} previous={7} />;
+}
 
 createRoot(document.getElementById('root')!).render(<App />);
